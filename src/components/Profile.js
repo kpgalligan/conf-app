2e791b4983311b3cb5c0de8246635331ebd1ac3d
Miss-Cheese <nat.galligan@gmail.com>
2020-05-29 18:51:54 -0400

    In progress: implementign Auth

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -4,8 +4,6 @@ class Profile extends React.Component {
 
     state = {
         editMode: false,
-        password: this.props.currentUser.password,
-        passwordConfirmation: "",
         name: this.props.currentUser.name,
         email: this.props.currentUser.email,
         company: this.props.currentUser.company,
@@ -28,37 +26,32 @@ class Profile extends React.Component {
 
     handleSubmit = (e) => {
         e.preventDefault()
-
-        if (this.state.password === this.state.passwordConfirmation) {
             
-            fetch(`${process.env.REACT_APP_API_CALL}/users/${this.props.currentUser.id}`, {
-                method: "PATCH",
-                headers: {
-                    "Content-Type": "application/json",
-                    "Accept": "application/json"
-                },
-                body: JSON.stringify({
-                    name: this.state.name,
-                    email: this.state.email,
-                    twitter_handle: this.state.twitter_handle,
-                    company: this.state.company,
-                    image_url: this.state.image_url,
-                    bio: this.state.bio,
-                    password: this.state.password
-                })
+        fetch(`${process.env.REACT_APP_API_CALL}/users/${this.props.currentUser.id}`, {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify({
+                name: this.state.name,
+                email: this.state.email,
+                twitter_handle: this.state.twitter_handle,
+                company: this.state.company,
+                image_url: this.state.image_url,
+                bio: this.state.bio,
             })
-            .then(response => response.json())
-            .then(response => {
-                if (response.errors) {
-                    alert(response.errors)
-                } else {
-                    this.props.setCurrentUser(response)
-                }
-            })
+        })
+        .then(response => response.json())
+        .then(response => {
+            if (response.errors) {
+                alert(response.errors)
+            } else {
+                this.props.setCurrentUser(response)
+            }
+        })
 
-        } else {
-            alert("Passwords don't match")
-        }
+      
     }
 
 
@@ -82,13 +75,13 @@ class Profile extends React.Component {
                     
                     <form onSubmit={this.handleSubmit}>
                         Name: <input name="name" value={this.state.name} onChange={this.handleChange}></input> <br></br>
-                        Email: <input name="email" value={this.state.email} onChange={this.handleChange} /> <br></br>
+                        {/* Email: <input name="email" value={this.state.email} onChange={this.handleChange} /> <br></br> */}
                         Company: <input name="company" value={this.state.company} onChange={this.handleChange}></input> <br></br>
                         Bio: <input name="bio" value={this.state.bio} onChange={this.handleChange}></input> <br></br>
                         Twitter Handle: <input name="twitter_handle" value={this.state.twitter_handle} onChange={this.handleChange}></input> <br></br>
                         Image URL:  <input name="image_url" value={this.state.image_url} onChange={this.handleChange}/> <br></br>
-                        New password: <input name="password" value={this.state.password} type="password" onChange={this.handleChange} /> <br></br>
-                        Confirm new password: <input name="passwordConfirmation" value={this.state.passwordConfirmation} type="password" onChange={this.handleChange}/> <br></br>
+                        {/* New password: <input name="password" value={this.state.password} type="password" onChange={this.handleChange} /> <br></br>
+                        Confirm new password: <input name="passwordConfirmation" value={this.state.passwordConfirmation} type="password" onChange={this.handleChange}/> <br></br> */}
                         <button type="submit">Update Profile</button> 
                     </form>
                     
