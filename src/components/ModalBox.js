314bbd2b1759c5823e79b5576a152a234eef134b
Kevin Galligan <kgalligan@gmail.com>
2020-06-07 11:45:33 -0400

    Fixing pop ups

--- src/components/ModalBox.js
+++ src/components/ModalBox.js
@@ -1,58 +1,64 @@
 import React from 'react';
-import Modal from 'react-modal';
+import {makeStyles} from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+import Button from '@material-ui/core/Button';
 
-class ModalBox extends React.Component {
-   
-    handleCloseModal = () => {
-      this.props.stopShowingPlayerInfo();
-    }
-    
-    render () {
-      return (
-        <div>
-          <Modal 
-             isOpen={this.props.showPlayerProfile ? true : false}
-             ariaHideApp={false}
-             shouldCloseOnEsc={true}
-             style={
-              { 
-              //   overlay: {
-              //   position: 'fixed',
-              //   width: '100px',
-              //   backgroundColor: 'rgba(255, 255, 255, 0.75)'
-              // }, 
-              content: {
-                // position: 'center',
-                // alignContent: 'center',
-                alignItems: 'center',
-                width: '400px',
-                height: '400px',
-                border: '1px solid #ccc',
-                background: '#fff',
-                overflow: 'auto',
-                WebkitOverflowScrolling: 'touch',
-                borderRadius: '4px',
-                outline: 'none',
-                padding: '50px'
-              } }}
-             contentLabel="Player Info" >
-               
-               {this.props.showPlayerProfile ? 
-                <>
-                  <div> { this.props.showPlayerProfile.name } </div> 
-                  <div> { this.props.showPlayerProfile.company || null } </div><br></br>
-                  <div> { this.props.showPlayerProfile.bio || null } </div><br></br>
-                  <div> { this.props.showPlayerProfile.image_url ? <img src={this.props.showPlayerProfile.image_url} height="250px" width="250px" alt="user"></img> : null }</div>
-                </>
-                : null}
-            
-            <button onClick={this.handleCloseModal}>Close</button>
-          </Modal>
-        </div>
-      );
-    }
-  }
-  
-  export default ModalBox
-  
+function getModalStyle() {
+    const top = 50;
+    const left = 50;
+
+    return {
+        top: `${top}%`,
+        left: `${left}%`,
+        transform: `translate(-${top}%, -${left}%)`,
+    };
+}
+
+function ModalBox(props) {
+
+    const useStyles = makeStyles((theme) => ({
+        paper: {
+            position: 'absolute',
+            width: 400,
+            backgroundColor: theme.palette.background.paper,
+            border: '2px solid #000',
+            boxShadow: theme.shadows[5],
+            padding: theme.spacing(2, 4, 3),
+        },
+    }));
+    const classes = useStyles();
+
+    return (
+
+        <Modal
+            open={props.showPlayerProfile}
+            aria-labelledby="simple-modal-title"
+            aria-describedby="simple-modal-description"
+        >
+            <div style={getModalStyle()} className={classes.paper}>
+                    {props.showPlayerProfile ?
+                        <>
+                            <h2 id="simple-modal-title">{props.showPlayerProfile.name}</h2>
+                            <p id="simple-modal-description"> { props.showPlayerProfile.name } <br/>
+                             { props.showPlayerProfile.company || null } <br/>
+                             { props.showPlayerProfile.bio || null } <br/>
+                             { props.showPlayerProfile.image_url ? <img src={props.showPlayerProfile.image_url} height="250px" width="250px" alt="user"></img> : null }
+                            </p>
+                        </>
+                        : null}
+
+                <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={() => props.stopShowingPlayerInfo()}
+                >
+                    Close
+                </Button>
+            </div>
+        </Modal>
+    );
+}
+
+export default ModalBox
+
   
--- src/components/VideoChatModal.js
+++ src/components/VideoChatModal.js
@@ -1,55 +1,64 @@
 import React from 'react';
-import Modal from 'react-modal';
+import Modal from '@material-ui/core/Modal';
 import Jitsi from "react-jitsi";
+import {makeStyles} from "@material-ui/core/styles";
 
-class VideoChatModal extends React.Component {
+/**
+ * Jitsi is basically 720p resolution, which is 1280w x 720h. If the screen is big enough,
+ * we show that exactly, or scale down to 70%. We could probably do this more adaptively, but
+ * this will need to be sufficient for today.
+ *
+ * @returns the width
+ */
+function calcWidth(){
+    return window.innerWidth >= 1350 ? 1280 : 896
+}
 
-    videoWidth = 950
-    videoHeight = 750
+function calcHeight(){
+    return window.innerHeight >= 800 ? 720 : 504
+}
 
-    videoClosingTrigger = (api) => api.addEventListener("readyToClose", () => this.props.hideVideoChat())
+function getModalStyle() {
+    const left = calcWidth()/2 + 38;
 
-    render() {
-        return (
-            <div>
-                {/* <button onClick={this.handleOpenModal}>Trigger Modal</button> */}
-                <Modal
-                    isOpen={this.props.videoChatRoomName}
-                    ariaHideApp={false}
-                    shouldCloseOnEsc={true}
-                    style={
-                        {
-                            //   overlay: {
-                            //   position: 'fixed',
-                            //   width: '100px',
-                            //   backgroundColor: 'rgba(255, 255, 255, 0.75)'
-                            // },
-                            content: {
-                                // position: 'center',
-                                // alignContent: 'center',
-                                alignItems: 'center',
-                                width: `${this.videoWidth + 20}px`,
-                                height: `${this.videoHeight + 20}px`,
-                                border: '1px solid #ccc',
-                                background: '#fff',
-                                overflow: 'auto',
-                                WebkitOverflowScrolling: 'touch',
-                                borderRadius: '4px',
-                                outline: 'none',
-                                padding: '10px'
-                            }
-                        }}
-                    contentLabel="Player Info">
+    return {
+        top: `20px`,
+        left: `50%`,
+        transform: `translate(-${left}px, 0px)`,
+    };
+}
 
+function VideoChatModal(props) {
+    const useStyles = makeStyles((theme) => ({
+        paper: {
+            position: 'absolute',
+            backgroundColor: theme.palette.background.paper,
+            border: '2px solid #000',
+            boxShadow: theme.shadows[5],
+            padding: theme.spacing(2, 4, 3),
+        },
+    }));
+    const classes = useStyles();
+
+    const videoClosingTrigger = (api) => api.addEventListener("readyToClose", () => props.hideVideoChat())
+
+    return (
+        <div>
+            <Modal
+                open={props.videoChatRoomName}
+                aria-labelledby="simple-modal-title"
+                aria-describedby="simple-modal-description"
+            >
+                <div style={getModalStyle()} className={classes.paper}>
+                    <h2 id="simple-modal-title">{props.videoChatRoomName}</h2>
                     <Jitsi
-                        onAPILoad={this.videoClosingTrigger}
-                        containerStyle={{width: `${this.videoWidth}px`, height: `${this.videoHeight}px`}}
-                        roomName={this.props.videoChatRoomName} displayName={this.props.videoChatRoomName}/>
-
-                </Modal>
-            </div>
-        );
-    }
+                        onAPILoad={videoClosingTrigger}
+                        containerStyle={{width: `${calcWidth()}px`, height: `${calcHeight()}px`}}
+                        roomName={props.videoChatRoomName} displayName={props.videoChatRoomName}/>
+                </div>
+            </Modal>
+        </div>
+    );
 }
 
 export default VideoChatModal
